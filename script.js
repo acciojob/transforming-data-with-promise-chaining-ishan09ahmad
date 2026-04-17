ec1a01b676ed180d8b33c1e992862da5016b13b7
Mohammad Ishan Ahmad <mdishanahmad@gmail.com>
2026-04-18 01:35:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,53 @@
-//your JS code here. If required.
+let inputEl = document.querySelector("#ip");
+let buttonEl = document.querySelector("#btn");
+let divEl = document.querySelector("#output");
+
+function showText(num) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(num);
+    }, 2000);
+  });
+}
+
+buttonEl.addEventListener("click", () => {
+  let num = inputEl.value.trim();
+  if (num.length !== 0) {
+    showText(num).then((res) => {
+      divEl.textContent = `Result: ${res}`;
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve(res);
+        }, 2000);
+      }).then((res) => {
+        res = res * 2;
+        divEl.textContent = `Result: ${res}`;
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve(res);
+          }, 1000);
+        }).then((res) => {
+          res = res - 3;
+          divEl.textContent = `Result: ${res}`;
+
+          return new Promise((resolve, reject) => {
+            setTimeout(() => {
+              resolve(res);
+            }, 1000);
+          }).then((res) => {
+            res = (res / 2).toFixed(1);
+            divEl.textContent = `Result: ${res}`;
+            return new Promise((resolve, reject) => {
+              setTimeout(() => {
+                resolve(res);
+              }, 1000);
+            }).then((res) => {
+              res = res + 10;
+              divEl.textContent = `Result: ${res}`;
+            });
+          });
+        });
+      });
+    });
+  }
+});
